2b0799423043083fe7a9157153e885cbd53189f4
Myles Megyesi <mylesmegyesi@me.com>
2013-09-20 20:29:07 -0500

    adds hack to fix git problems

--- site-cookbooks/dotfiles/recipes/default.rb
+++ site-cookbooks/dotfiles/recipes/default.rb
@@ -1,11 +1,24 @@
 
+DOTFILES_USER_HOME = node['dotfiles']['user_home']
 dotfiles_dir = File.join(node['dotfiles']['user_home'], '.dotfiles')
+
+# HACK until https://tickets.opscode.com/browse/CHEF-3940 is resolved
+class Chef::Provider::MyGit < Chef::Provider::Git
+  alias :run_options_old :run_options
+  def run_options(run_opts={})
+    ret = run_options_old(run_opts)
+    ret[:environment] = {} unless ret[:environment]
+    ret[:environment]['HOME'] = DOTFILES_USER_HOME
+    ret
+  end
+end
 
 git dotfiles_dir do
   repository 'git@github.com:mylesmegyesi/dotfiles.git'
   action :sync
   user node['dotfiles']['username']
   group node['dotfiles']['username']
+  provider Chef::Provider::MyGit
 end
 
 bash 'Install the dotfiles' do
