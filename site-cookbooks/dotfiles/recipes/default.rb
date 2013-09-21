70d04b3ec91a841b997b9ff77f4f9e62165a9779
Myles Megyesi <mylesmegyesi@me.com>
2013-09-20 20:31:53 -0500

    try one more thing before resorting to the hack

--- site-cookbooks/dotfiles/recipes/default.rb
+++ site-cookbooks/dotfiles/recipes/default.rb
@@ -1,16 +1,8 @@
 
-DOTFILES_USER_HOME = node['dotfiles']['user_home']
 dotfiles_dir = File.join(node['dotfiles']['user_home'], '.dotfiles')
 
-# HACK until https://tickets.opscode.com/browse/CHEF-3940 is resolved
-class Chef::Provider::MyGit < Chef::Provider::Git
-  alias :run_options_old :run_options
-  def run_options(run_opts={})
-    ret = run_options_old(run_opts)
-    ret[:environment] = {} unless ret[:environment]
-    ret[:environment]['HOME'] = DOTFILES_USER_HOME
-    ret
-  end
+directory dotfiles_dir do
+  recursive true
 end
 
 git dotfiles_dir do
@@ -18,7 +10,6 @@ git dotfiles_dir do
   action :sync
   user node['dotfiles']['username']
   group node['dotfiles']['username']
-  provider Chef::Provider::MyGit
 end
 
 bash 'Install the dotfiles' do
