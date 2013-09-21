180ae8ab3532e2f34c25606d81c742cceed9d675
Myles Megyesi <mylesmegyesi@me.com>
2013-09-20 19:12:53 -0500

    adds chruby

--- roles/vm.rb
+++ roles/vm.rb
@@ -1,3 +1,5 @@
+require 'etc'
+
 name 'vm'
 run_list [
   'recipe[build-essential]',
@@ -7,7 +9,20 @@ run_list [
   'recipe[zsh]'
 ]
 
+USERNAME  = 'mylesmegyesi'
+
+log '*' * 8
+log Etc.getpwnam(USERNAME)
+log '*' * 8
+
+USER_HOME = "/home/#{USERNAME}"
+ZSH_DIR   = File.join(USER_HOME, '.zsh')
+
 default_attributes({
+  :chruby => {
+    :sh_dir  => ZSH_DIR,
+    :sh_name => 'chruby.zsh'
+  },
   :mysql => {
     :client => {
       :packages => %w(mysql-client libmysqlclient-dev ruby-mysql)
@@ -15,5 +30,5 @@ default_attributes({
     :server_root_password   => '',
     :server_repl_password   => '',
     :server_debian_password => ''
-  }
+  },
 })
